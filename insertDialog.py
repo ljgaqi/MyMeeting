9e24b4e23a53eccdd1a2f5e4eea2e1c8e17a6542
ljgaqi <liaqi@163.com>
2021-09-23 11:03:03 +0800

    初步完善弹出窗口，下一步要弄清楚窗口间的参数传递！

--- insertDialog.py
+++ insertDialog.py
@@ -1,6 +1,6 @@
 import datetime
 import sys
-
+import time
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
@@ -17,7 +17,14 @@ class insertDialog(QDialog,Ui_Dialog):
         self.buttonBox.button(QDialogButtonBox.Cancel).setText('取消')
         self.dateEdit.setDate(datetime.date.today())
 
-
+        datestr=self.dateEdit.date().toString()
+        roomstr=self.comboBox.currentText()
+        namestr=self.lineEdit.text()
+        timestr=self.comboBox_3.currentText()
+        typestr=self.comboBox_2.currentText()
+        comestr=self.lineEdit_2.text()
+        datelist=[datestr+timestr,namestr,roomstr,comestr,typestr]
+        print(datelist)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
