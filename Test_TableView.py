8fe86915d530cb7da1a6bc8294ca470a0f84c6c7
ljgaqi <liaqi@163.com>
2021-09-29 12:53:35 +0800

    开始实现插入数据库数据功能

---
+++ Test_TableView.py
@@ -0,0 +1,43 @@
+import sys
+from PyQt5.QtSql import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+
+class Test_TableView(QWidget):
+    def __init__(self, parent=None):
+        super(Test_TableView, self).__init__(parent)
+        self.setWindowTitle("TableView连接数据库窗口")
+        self.resize(800, 600)
+
+        self.connectDB()
+        self.searchDB()
+        self.createTable()
+
+    def connectDB(self):
+        self.db = QSqlDatabase.addDatabase('QSQLITE')
+        self.db.setDatabaseName('./meeting.db')
+        self.db.open()
+
+    def searchDB(self):
+        # self.model = QSqlTableModel()
+        # self.model.setTable('DateMeeting')
+        # self.model.select()
+        self.model = QSqlQueryModel()
+        self.model.setQuery("select * from DateMeeting where datetime > '2021-09-28 8:00:00:000' and datetime < '2021-10-22 8:00:00:000'")
+
+    def createTable(self):
+        self.tableview = QTableView()
+        self.tableview.setModel(self.model)
+        layout = QVBoxLayout()
+        layout.addWidget(self.tableview)
+        self.setLayout(layout)
+
+
+
+
+if __name__=="__main__":
+    app = QApplication(sys.argv)
+    table = Test_TableView()
+    table.show()
+    sys.exit(app.exec_())
--- Time_Test.py
+++ Time_Test.py
@@ -5,14 +5,20 @@ db = QSqlDatabase.addDatabase('QSQLITE')
 db.setDatabaseName('./meeting.db')
 db.open()
 
-datelist = ['testname', '2021-09-28 9:00:00:000', '会议室一', 'test', '政府采购']
+# datelist = ['testname', '2021-09-28 9:00:00:000', '会议室一', 'test', '政府采购']
+# query = QSqlQuery()
+# query.prepare("Insert Into DateMeeting (name,datetime,room,company,type) values (?,?,?,?,?)")
+#
+# for list in datelist:
+#     query.addBindValue(list)
+
 query = QSqlQuery()
-query.prepare("Insert Into DateMeeting (name,datetime,room,company,type) values (?,?,?,?,?)")
 
-for list in datelist:
-    query.addBindValue(list)
+query.exec("select * from DateMeeting where datetime > '2021-09-28 8:00:00:000'")
 
-query.exec()
+while(query.next()):
+    for i in range(6):
+        print(query.value(i))
 
 db.commit()
 db.close()
